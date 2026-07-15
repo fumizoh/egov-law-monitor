b4344b628cba2fd35f74d8157d379c22612b0708
oogushi <oogushi868@gmail.com>
2026-07-15 13:35:30 +0900

    feat: add law view module

---
+++ src/law_view.py
@@ -0,0 +1,18 @@
+"""
+Law view.
+"""
+
+from models import Event, Law
+
+
+def create_law_view(
+    events: list[Event],
+) -> list[Law]:
+    """
+    Create a Law view from events.
+
+    This is a placeholder implementation.
+    The actual conversion logic will be added later.
+    """
+
+    return []
